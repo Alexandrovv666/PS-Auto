e17e507f5568f9fea468c1ae9148ba73ac94537f
Serg <arioxp.debian@gmail.com>
2017-11-23 18:21:36 +0300

    fix

--- jmeter.ps1
+++ jmeter.ps1
@@ -1,87 +1,45 @@
-	cd C:\JMeter\bin\
-
 	$root_folder = "C:\Data\"
-	$Directory = "C:\Data\"
-	cls;
 
-#Get list of projects
 
 	Write-Host 'list of projects:'
-	$list_projects = Get-ChildItem $Directory -Directory -Name;
+	$list_projects = Get-ChildItem $root_folder -Directory -Name;
 	$count = 0;
 	$Project_Name = "";
+
+
 	foreach ($i in $list_projects) {
 		$Project_Name = $list_projects[$count];
 		Write-Host "   "$count"   " $Project_Name;
 		$count = 1 + $count;
 	}
+	$num_project = Read-Host 'Select project'
 
-	$num_project = Read-Host 'Input number '
 	$Project_Name = $list_projects[$num_project];
 
-	$Directory = "" + $root_folder + $Project_Name + "\"
-
-
-#Get availebe test-plans of the project
-
-
-	echo $Directory
+  
 	$count = 0;
-	$list_tests = Get-ChildItem $Directory -Include *.jmx -File -recurse -Name
+	$list_tests = Get-ChildItem ($root_folder + $Project_Name + "\") -Include *.jmx -File -recurse -Name
 	foreach ($i in $list_tests) {
-		$Test_Name = $Directory + $list_tests[$count];
-		Write-Host "   "$count"   " $Test_Name;
+		$Test_Name =  $list_tests[$count];
+		Write-Host "   "$count"   " $Test_Name.split("\")[0];
 		$count = 1 + $count;
 	}
 
-	$num_project = Read-Host 'Выберите тест '
-	Write-Host 'тест '$Test_Name;
+	$num_test   = Read-Host 'select mode: '
+    $FullPathToTest = $root_folder + $Project_Name + "\" + $list_tests[$num_test];
+	$Test_Name  = $list_tests[$num_test].Split("\")[0];
+    $UID_Name   = $list_tests[$num_test].Split("\")[1].split(".")[0];
+    cls
+   	Write-Host 'Project '$Project_Name;
+	Write-Host 'Test    '$Test_Name;
+
+	$log_destination = $root_folder + $Project_Name + "\" + $Test_Name + "\" + $UID_Name + "." + (get-date -f 'dd.MM.yyyy..HH.mm.ss') +".log"
+
+    do { $answer = Read-Host "y or n" } 
+    until ("y","n" -ccontains $answer)
 
 
-
-	$directory_destination = $Directory + "\" + (get-date -f 'dd.MM.yyyy') + "\"
-	$log_destination = $directory_destination + "log.log"
-
-	if (!(Test-Path $directory_destination)) {
-		New-Item -Path $directory_destination -ItemType "Directory"
-	}
-
-pause;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cd C:\JMeter\bin
+    jmeter.bat -n -t $FullPathToTest -l $log_destination
 
 
